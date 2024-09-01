9bf6e1e29a9e236a20a0c40c743fcdd5adfb443b
Modibo KEITA <modykeita2@gmail.com>
2024-09-01 17:37:48 +0200

    created new view

---
+++ TravelVista/TravelVista/Views/CellView.swift
@@ -0,0 +1,16 @@
+//
+//  CellView.swift
+//  TravelVista
+//
+//  Created by KEITA on 01/09/2024.
+//
+
+import SwiftUI
+
+struct CellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
---
+++ TravelVista/TravelVista/Views/ListSwiftUIView.swift
@@ -0,0 +1,30 @@
+//
+//  ListSwiftUIView.swift
+//  TravelVista
+//
+//  Created by KEITA on 01/09/2024.
+//
+
+import SwiftUI
+
+struct ListSwiftUIView: View {
+    var body: some View {
+        List{
+            NavigationLink {
+                <#code#>
+            } label: {
+                CellView()
+            }
+
+        }
+    }
+}
+
+struct DetailView : UIViewControllerRepresentable{
+    func makeUIViewController(context: Context) -> some UIViewController {
+        <#code#>
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        <#code#>
+    }
+}
