b7bfdf2b1c09604ac4d67ec30a02a6de0ff9487d
Modibo KEITA <modykeita2@gmail.com>
2024-09-01 16:32:26 +0200

    created new view

---
+++ TravelVista/TravelVista/Views/TitleView.swift
@@ -0,0 +1,20 @@
+//
+//  TitleView.swift
+//  TravelVista
+//
+//  Created by KEITA on 01/09/2024.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Vietnam").font(.title)
+            }
+        }
+    }
+}
+
+
