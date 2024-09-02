d2c7c006a6569d418e5b2b6c9184e58c5b01078a
Modibo KEITA <modykeita2@gmail.com>
2024-09-02 11:47:12 +0200

    initialise makeUIViewController with controller

--- TravelVista/TravelVista/Views/ListSwiftUIView.swift
+++ TravelVista/TravelVista/Views/ListSwiftUIView.swift
@@ -10,21 +10,28 @@ import SwiftUI
 struct ListSwiftUIView: View {
     var body: some View {
         List{
-//            NavigationLink() {
-//                <#code#>
-//            } label: {
-//                CellView()
-//            }
+           NavigationLink() {
+               <#code#>
+          } label: {
+              CellView()
+            }
 
         }
     }
 }
-//
-//struct DetailView : UIViewControllerRepresentable{
-//    func makeUIViewController(context: Context) -> some UIViewController {
-////        guard let detailVC = UIStoryboard(name: <#T##String#>, bundle: <#T##Bundle?#>)
-//    }
-//    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-//        <#code#>
-//    }
-//}
+
+struct DetailView : UIViewControllerRepresentable{
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        
+        guard let detailVC = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else {
+            fatalError()
+        }
+        
+        return detailVC
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        <#code#>
+    }
+}
