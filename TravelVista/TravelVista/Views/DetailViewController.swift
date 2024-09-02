f06e419a8cf196cd7d1100b96e0cf69aa4c55759
Modibo KEITA <modykeita2@gmail.com>
2024-09-02 16:16:06 +0200

    update element

--- TravelVista/TravelVista/Views/DetailViewController.swift
+++ TravelVista/TravelVista/Views/DetailViewController.swift
@@ -10,17 +10,22 @@ import MapKit
 import SwiftUI
 
 class DetailViewController: UIViewController, MKMapViewDelegate {
+    @IBOutlet weak var countryNameLabel: UILabel!
+    @IBOutlet weak var capitalNameLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var mapButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var embedMapView: UIView!
+    @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var rateView: UIView!
     
     var country: Country?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //TitleView
         let hostingController = UIHostingController(rootView: TitleView(country: country))
         
         self.addChild(hostingController)
@@ -31,10 +36,13 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
         hostingController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
         hostingController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
         
-        
         hostingController.view.heightAnchor.constraint(equalToConstant: 19).isActive = true
         hostingController.view.widthAnchor.constraint(equalToConstant: 19).isActive = true
-      
+        hostingController.view.centerYAnchor.constraint(equalTo: self.rateView.centerYAnchor).isActive = true
+        
+        self.titleView.isHidden = true
+        self.rateView.isHidden = true
+        self.capitalNameLabel.isHidden = true
         
         self.setCustomDesign()
 
@@ -46,11 +54,14 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
     private func setUpData(country: Country) {
         self.title = country.name
         
+        self.countryNameLabel.text = country.name
+//        self.capitalNameLabel.text = country.capital
         self.imageView.image = UIImage(named: country.pictureName )
         self.descriptionTextView.text = country.description
         
+//        self.setRateStars(rate: country.rate)
         self.setMapLocation(lat: self.country?.coordinates.latitude ?? 28.394857,
-                            long: self.country?.coordinates.longitude ?? 84.124008)
+            long: self.country?.coordinates.longitude ?? 84.124008)
     }
     
     private func setCustomDesign() {
@@ -70,7 +81,7 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
         self.mapView.delegate = self
     }
     
-   
+    
     
     // Cette fonction est appelée lorsque la carte est cliquée
     // Elle permet d'afficher un nouvel écran qui contient une carte
