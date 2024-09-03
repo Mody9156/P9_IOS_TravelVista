3b865d7673ba78fe8eb59501c3ccbb3f4aa7a772
Modibo KEITA <modykeita2@gmail.com>
2024-09-03 10:03:37 +0200

    delete Vstack

--- TravelVista/TravelVista/Views/CellView.swift
+++ TravelVista/TravelVista/Views/CellView.swift
@@ -9,35 +9,33 @@ import SwiftUI
 
 struct CellView: View {
     var body: some View {
-        VStack {
             List {
-                    Section (header:
-                                Text("Asie")){
-                        HStack{
-                            Image("vietnam")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 50, height: 50)
-                                .clipShape(Circle())
-                            
-                            VStack(alignment: .leading,spacing: 5){
-                                Text("Vietnam")
-                                    .font(.title)
-                                    .foregroundColor(.blue)
-                                Text("Hanoi")
-                                    .foregroundColor(.black)
-                            }
-                            Spacer()
-                            Text("4")
-                            Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
-                        }
-                        .padding(.vertical, 5)
+                Section (header:
+                            Text("Asie")){
+                    HStack{
+                        Image("vietnam")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .clipShape(Circle())
                         
+                        VStack(alignment: .leading,spacing: 5){
+                            Text("Vietnam")
+                                .font(.title)
+                                .foregroundColor(.blue)
+                            Text("Hanoi")
+                                .foregroundColor(.black)
+                        }
+                        Spacer()
+                        Text("4")
+                        Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
                     }
-                     .listSectionSeparatorTint(.black)
+                    .padding(.vertical, 5)
+                    
                 }
+                 .listSectionSeparatorTint(.black)
+            }
             .listStyle(.insetGrouped).background(Color(.clear))
-        }
            
         }
         
