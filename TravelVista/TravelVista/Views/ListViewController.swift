4e1e3b229919f9ef5bab128927af923ce4998d1a
Modibo KEITA <modykeita2@gmail.com>
2024-09-03 13:47:28 +0200

    delete code

--- TravelVista/TravelVista/Views/ListViewController.swift
+++ TravelVista/TravelVista/Views/ListViewController.swift
@@ -1,77 +1,77 @@
+////
+////  ListViewController.swift
+////  TravelVista
+////
+////  Created by Amandine Cousin on 13/12/2023.
+////
 //
-//  ListViewController.swift
-//  TravelVista
+//import UIKit
+//import SwiftUI
 //
-//  Created by Amandine Cousin on 13/12/2023.
+//class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//    @IBOutlet weak var tableview: UITableView!
+//    
+//    var regions: [Region] = Service().load("Source.json")
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+////        self.title = "Liste de voyages"
+//        
+//        //tableview
+//        let hostingController  = UIHostingController(rootView: ListView())
+//        
+//        self.addChild(hostingController)
+//        self.view.addSubview(hostingController.view)
+//        hostingController.didMove(toParent: self)
+//        
+//        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        NSLayoutConstraint.activate([
+//            hostingController.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+//            hostingController.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+//            hostingController.view.topAnchor.constraint(equalTo: self.view.topAnchor),
+//            hostingController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+//        ])
+//        
+//        self.tableview.isHidden = true
+////        self.tableview.dataSource = self
+////        self.tableview.delegate = self
+//    }
+//    
+//    // Gestion des sections - 2 fonctions
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return regions.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return regions[section].name
+//    }
+//    
+//    // Gestion des cellules - 3 fonctions
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return regions[section].countries.count
+//    }
+//    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell: CustomCell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+//        cell.setUpCell(country: regions[indexPath.section].countries[indexPath.row])
+//        return cell
+//    }
+//    
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 65
+//    }
 //
-
-import UIKit
-import SwiftUI
-
-class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var tableview: UITableView!
-    
-    var regions: [Region] = Service().load("Source.json")
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        self.title = "Liste de voyages"
-        
-        //tableview
-        let hostingController  = UIHostingController(rootView: ListView())
-        
-        self.addChild(hostingController)
-        self.view.addSubview(hostingController.view)
-        hostingController.didMove(toParent: self)
-        
-        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            hostingController.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            hostingController.view.topAnchor.constraint(equalTo: self.view.topAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
-        ])
-        
-        self.tableview.isHidden = true
-//        self.tableview.dataSource = self
-//        self.tableview.delegate = self
-    }
-    
-    // Gestion des sections - 2 fonctions
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return regions.count
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return regions[section].name
-    }
-    
-    // Gestion des cellules - 3 fonctions
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return regions[section].countries.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: CustomCell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
-        cell.setUpCell(country: regions[indexPath.section].countries[indexPath.row])
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 65
-    }
-
-    
-    // Fonction appelée lorsqu'une cellule est cliquée
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let senderCell = sender as? CustomCell,
-           let country = senderCell.country,
-           let controller: DetailViewController = segue.destination as? DetailViewController {
-            controller.country = country
-            
-        }
-    }
-}
-
+//    
+//    // Fonction appelée lorsqu'une cellule est cliquée
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if let senderCell = sender as? CustomCell,
+//           let country = senderCell.country,
+//           let controller: DetailViewController = segue.destination as? DetailViewController {
+//            controller.country = country
+//            
+//        }
+//    }
+//}
+//
