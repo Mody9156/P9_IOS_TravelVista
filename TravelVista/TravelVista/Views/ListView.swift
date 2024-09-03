0fcd9097ef7d5d5d2528fdf34f2764915e5de70f
Modibo KEITA <modykeita2@gmail.com>
2024-09-03 18:32:18 +0200

    created new view

--- TravelVista/TravelVista/Views/ListView.swift
+++ TravelVista/TravelVista/Views/ListView.swift
@@ -18,32 +18,8 @@ struct ListView: View {
                     Section(header: Text(region.name)) {
                         // Boucle sur chaque pays dans la région
                         ForEach(region.countries, id: \.name) { country in
-                            // NavigationLink pour chaque pays
-                            NavigationLink(destination: DetailView(country:country) {
-                                HStack {
-                                    Image(country.pictureName)
-                                        .resizable()
-                                        .frame(width: 50, height: 50)
-                                        .clipShape(Circle())
-                                    
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text(country.name)
-                                            .font(.title2)
-                                            .foregroundColor(Color("CustomBlue"))
-                                        Text(country.capital)
-                                            .foregroundColor(.black)
-                                    }
-                                    Spacer()
-                                    Text("\(country.rate)")
-                                  
-                                        Image(systemName: "star.fill")
-                                            .resizable()
-                                            .aspectRatio(contentMode: .fit)
-                                            .frame(width: 25, height: 25)
-                                            .foregroundColor(Color("AccentColor"))
-                                    
-                                }
-                            }
+                           //NavLink
+                            NavigationView(country: country)
                         }
                     }
                 }
@@ -73,5 +49,39 @@ struct DetailView : UIViewControllerRepresentable{
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
         //Update the user interface (SwiftUI) when you have an interaction
         //Our interface is static
+    }
+}
+
+
+struct NavigationView : View {
+    var country : Country
+    
+    var body : some View{
+        // NavigationLink pour chaque pays
+        NavigationLink(destination: DetailView(country:country)) {
+            HStack {
+                Image(country.pictureName)
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text(country.name)
+                        .font(.title2)
+                        .foregroundColor(Color("CustomBlue"))
+                    Text(country.capital)
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                Text("\(country.rate)")
+              
+                    Image(systemName: "star.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(Color("AccentColor"))
+                
+            }
+        }
     }
 }
