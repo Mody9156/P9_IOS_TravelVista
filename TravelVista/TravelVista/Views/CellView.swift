73ab06995251dadc8b7268c999ef647427c6b78d
Modibo KEITA <modykeita2@gmail.com>
2024-09-03 10:03:14 +0200

    add vstask

--- TravelVista/TravelVista/Views/CellView.swift
+++ TravelVista/TravelVista/Views/CellView.swift
@@ -9,83 +9,35 @@ import SwiftUI
 
 struct CellView: View {
     var body: some View {
+        VStack {
             List {
-                Section (header:
-                            Text("Asie")){
-                    HStack{
-                        Image("vietnam")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                        
-                        VStack(alignment: .leading,spacing: 5){
-                            Text("Vietnam")
-                                .font(.title)
-                                .foregroundColor(.blue)
-                            Text("Hanoi")
-                                .foregroundColor(.black)
+                    Section (header:
+                                Text("Asie")){
+                        HStack{
+                            Image("vietnam")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .clipShape(Circle())
+                            
+                            VStack(alignment: .leading,spacing: 5){
+                                Text("Vietnam")
+                                    .font(.title)
+                                    .foregroundColor(.blue)
+                                Text("Hanoi")
+                                    .foregroundColor(.black)
+                            }
+                            Spacer()
+                            Text("4")
+                            Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
                         }
-                        Spacer()
-                        Text("4")
-                        Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
-                    }
-                    .padding(.vertical, 5)
-                    
-                }
-                 .listSectionSeparatorTint(.black)
-                
-                Section (header:
-                            Text("Asie")){
-                    HStack{
-                        Image("vietnam")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
+                        .padding(.vertical, 5)
                         
-                        VStack(alignment: .leading,spacing: 5){
-                            Text("Vietnam")
-                                .font(.title)
-                                .foregroundColor(.blue)
-                            Text("Hanoi")
-                                .foregroundColor(.black)
-                        }
-                        Spacer()
-                        Text("4")
-                        Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
                     }
-                    .padding(.vertical, 5)
-                    
+                     .listSectionSeparatorTint(.black)
                 }
-                 .listSectionSeparatorTint(.black)
-                
-                Section (header:
-                            Text("Asie")){
-                    HStack{
-                        Image("vietnam")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50)
-                            .clipShape(Circle())
-                        
-                        VStack(alignment: .leading,spacing: 5){
-                            Text("Vietnam")
-                                .font(.title)
-                                .foregroundColor(.blue)
-                            Text("Hanoi")
-                                .foregroundColor(.black)
-                        }
-                        Spacer()
-                        Text("4")
-                        Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
-                    }
-                    .padding(.vertical, 5)
-                    
-                }
-                 .listSectionSeparatorTint(.black)
-            }
             .listStyle(.insetGrouped).background(Color(.clear))
+        }
            
         }
         
