570666e79b6696df9ad9a87399b15b391a615f90
Modibo KEITA <modykeita2@gmail.com>
2024-09-03 09:53:15 +0200

    delete constante and real element

--- TravelVista/TravelVista/Views/TitleView.swift
+++ TravelVista/TravelVista/Views/TitleView.swift
@@ -8,33 +8,29 @@
 import SwiftUI
 
 
-
 struct TitleView: View {
-    var country : Country?
     
     var body: some View {
         
         HStack {
-            if let country = country{
                 
                 VStack(alignment: .leading) {
                     
                     
-                    Text(country.name)
+                    Text("Norvège")
                         .font(.title)
                         .foregroundColor(.blue)
                     
                     
-                    Text(country.capital)
+                    Text("Oslo")
                         .foregroundColor(.gray)
                 }
                 Spacer()
-                ForEach(0..<country.rate,id: \.self){ _ in
+                ForEach(0..<4){ _ in
                     Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
                 }
                
                 
-            }
             
         }
         .padding()
