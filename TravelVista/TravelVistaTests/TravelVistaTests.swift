d8ad65d937a3e6b2f671340f5230ac04aa87988a
Modibo KEITA <modykeita2@gmail.com>
2024-09-05 15:00:03 +0200

    add new testUnitaire

---
+++ TravelVista/TravelVistaTests/TravelVistaTests.swift
@@ -0,0 +1,20 @@
+//
+//  TravelVistaTests.swift
+//  TravelVistaTests
+//
+//  Created by KEITA on 05/09/2024.
+//
+
+import XCTest
+@testable import TravelVista
+
+final class TravelVistaTests: XCTestCase {
+
+    
+
+    func testWhenRegionsIsEMmpty() throws {
+       
+    }
+
+
+}
