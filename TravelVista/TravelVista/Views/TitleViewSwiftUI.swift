81f06c581caddf0b4f5bbb0c02cf56513880bb10
Modibo KEITA <modykeita2@gmail.com>
2024-09-05 18:02:41 +0200

    created new file and struct

---
+++ TravelVista/TravelVista/Views/TitleViewSwiftUI.swift
@@ -0,0 +1,43 @@
+//
+//  TitleView.swift
+//  TravelVista
+//
+//  Created by KEITA on 01/09/2024.
+//
+
+import SwiftUI
+
+struct TitleViewSwiftUI: View {
+    let country : Country?
+    
+    var body: some View {
+        
+        VStack {
+            if let country = country {
+                
+                HStack {
+                    
+                    VStack(alignment: .leading) {
+                        
+                        Text(country.name)
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(Color("CustomBlue"))
+                        
+                        Text(country.capital)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    ForEach(0..<country.rate){ _ in
+                        Image(systemName:  "star.fill").foregroundColor(Color("AccentColor"))
+                    }
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+
