af6769f074f93b5463526cfce34ccad74214692f
Modibo KEITA <modykeita2@gmail.com>
2024-09-05 18:06:01 +0200

    initialise new tests

--- TravelVista/TravelVistaTests/TravelVistaTests.swift
+++ TravelVista/TravelVistaTests/TravelVistaTests.swift
@@ -9,27 +9,27 @@ import XCTest
 
 final class TravelVistaTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testWhenRegionsIsNotEmpty() throws {
+       //Given
+        let travelViewModel = TravelViewModel()
+        
+        //When
+        let result = travelViewModel.regions
+        
+        //Then
+        XCTAssertEqual(result.isEmpty, false)
+        XCTAssertEqual(result.first?.name == "Europe", true)
+ 
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testWhenRegionsNotEmpty() throws {
+        //Given
+        let travelViewModel = TravelViewModel()
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        //When
+        var result = travelViewModel.regions
+        result.removeAll()
+        //Then
+        XCTAssertEqual(result.isEmpty, true)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
